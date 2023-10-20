340c2199e0e71bb6c6bbc3f023ad3cbd4fd99d3f
emunkhsaikhan <emonkhsaikhan@gmail.com>
2023-10-20 08:56:08 +0800

    description yoronhii zagvar saijruulalt

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var fearless = document.getElementById('fearless');
 var favorite = document.getElementById('favorite');
 var container = document.querySelector('.container');
+var bgBlack = document.querySelector('.black');
 
 function slideInFearless() {
     fearless.style.transition = '1000ms ease-in-out';
-    fearless.style.left = "15%";
-}
-function slideOutFearless() {
-    fearless.style.left = '-100%'
+    fearless.style.left === "-100%" ? fearless.style.left = "15%" : fearless.style.left = "-100%";
+    fearless.style.left === "-100%" ? bgBlack.style.opacity = "0" : bgBlack.style.opacity = "0.5";
+    fearless.style.left === "-100%" ? bgBlack.style.zIndex = "-10" : bgBlack.style.zIndex = "10";
 }
 favorite.addEventListener('click', function(e) {
     if(favorite.style.background === 'none') {
