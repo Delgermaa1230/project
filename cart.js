8d26fcc815ce5ec0a0b72379fd7f17b0e6c99d66
Zadyy <nyambayar2014@gmail.com>
2023-10-22 19:19:28 +0800

    total update func

--- cart.js
+++ cart.js
@@ -1,1 +1,26 @@
-console.log("Test")
+
+
+var removeCartItemBtns = document.getElementsByClassName('btn-danger')
+for (var i=0; i< removeCartItemBtns.length; i++) {
+    var button = removeCartItemBtns[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var k=0; k<cartRows.length; k++) {
+        var cartRow = cartRows[k]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+        console.log(total)
+    }
+}
