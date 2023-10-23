d97dd43dd40b5a94f897d26c9d9c3c2e67200b92
Zadyy <nyambayar2014@gmail.com>
2023-10-23 13:51:27 +0800

    added cart table

--- cart.js
+++ cart.js
@@ -12,41 +12,42 @@ function ready() {
         var button = removeCartItemBtns[i]
         button.addEventListener('click', function (event) {
             var buttonClicked = event.target
-            buttonClicked.parentElement.parentElement.remove()
+            buttonClicked.parentElement.remove()
             updateCartTotal()
         })
     }
 
-    var quanityInput = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quanityInput.length; i++) {
-        var input = quanityInput[i]
-        input.addEventListener('change', quanityChanged)
-        updateCartTotal()
-    }
-}
+//     var quanityInput = document.getElementsByClassName('cart-quantity-input')
+//     for (var i = 0; i < quanityInput.length; i++) {
+//         var input = quanityInput[i]
+//         input.addEventListener('change', quanityChanged)
+//         updateCartTotal()
+//     }
+// }
 
-function quanityChanged(event) {
-    var input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
-    }
-    updateCartTotal()
-}
+// function quanityChanged(event) {
+//     var input = event.target
+//     if (isNaN(input.value) || input.value <= 0) {
+//         input.value = 1
+//     }
+//     updateCartTotal()
+// }
 
-function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-item')
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    console.log(cartRows)
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+// function updateCartTotal() {
+//     var cartItemContainer = document.getElementsByClassName('cart-item')
+//     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+//     console.log(cartRows)
+//     for (var i = 0; i < cartRows.length; i++) {
+//         var cartRow = cartRows[i]
+//         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+//         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
         
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        // total = total + (price * quantity)
-        // console.log(price)
-        // console.log(total)
-    }
-    // document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
-}
+//         var price = parseFloat(priceElement.innerText.replace('$', ''))
+//         var quantity = quantityElement.value
+//         // total = total + (price * quantity)
+//         // console.log(price)
+//         // console.log(total)
+//     }
+//     // document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+// }
+
